684e3f7579e3c47e017cb8ba629b6bbd6dd19d61
MohammadHH <mhmd98.home@gmail.com>
2024-01-08 04:10:57 +0200

    feat: add icon components

---
+++ src/Icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as MenuIcon } from './BarsIcon';
+export { default as CloseIcon } from './CloseIcon';
+export { default as CameraIcon } from './CameraIcon';
+export { default as UploadIcon } from './UploadIcon';
+export { default as LoaderIcon } from './LoaderIcon';
