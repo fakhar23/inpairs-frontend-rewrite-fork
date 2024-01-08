3812163b9279a2f44ed1a0b3a43161029f30840d
MohammadHH <mhmd98.home@gmail.com>
2024-01-08 04:15:18 +0200

    feat: update tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,93 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    animation: {
+      zoom: "zoom 2s ease-in-out infinite",
+      spin: "spin 1s linear infinite",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
+    },
+    extend: {
+      textColor: {
+        purple: "#622466",
+        red: "#Ef3E37",
+        light: {
+          black: "#3D3C3C",
+        },
+        gray: {
+          charcoal: "#5B5B5B",
+          gunmetal: "#3D3C3C",
+        },
+      },
+      keyframes: {
+        spin: {
+          from: {
+            transform: "rotate(0deg)",
+          },
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+      },
+      backgroundImage: {
+        circles: "url('/circles.svg')",
+        profile: "url('/profileBg.png')",
+        about: "url('/Group-5.svg')",
+      },
+      boxShadow: {
+        "custom-shadow":
+          "0 0px 30px 0 rgba(0,0,0,.1), 0 1px 2px -1px rgba(0,0,0,.1)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      height: {
+        0.8: "0.8px",
+      },
+      fontFamily: {
+        bryantProRegular: ["Bryant Pro Regular", "system-ui", "sans-serif"],
+        bryantProLight: ["Bryant Pro Light", "system-ui", "sans-serif"],
+        bryantProMedium: ["Bryant Pro Medium", "system-ui", "sans-serif"],
+        bryantProBold: ["Bryant Pro Bold", "system-ui", "sans-serif"],
+        funky: ["Funky", "system-ui", "sans-serif"],
+        inter: ["Inter", "system-ui", "sans-serif"],
+        pacifico: ["Pacifico", "cursive", "system-ui", "sans-serif"],
+      },
+      fontSize: {
+        xsmall: "12px",
+        footer: "14px",
+        small: "24px",
+        large: "40px",
+        xlarge: "48px",
+        regular: "16px",
+      },
+    },
+    // borderWidth: {
+    //   DEFAULT: '1px'
+    // },
+    screens: {
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      ld: { max: "1000px" },
+      md: { max: "767px" },
+      ms: { max: "560px" },
+      sm: { max: "420px" },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
