16cbc7769931459df582e43896574fbf23f6c93d
fakhar23 <muhammadfakhar111@gmail.com>
2024-01-08 21:24:23 +0500

    added coming soon page

---
+++ src/app/coming-soon/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+import { randomId, useTimeout } from "@mantine/hooks";
+import { useState } from "react";
+import Gate from "./components/Gate";
+
+const ComingSoonPage = () => {
+  const [temp, setTemp] = useState("");
+  useTimeout(() => setTemp(randomId()), 1000, {
+    autoInvoke: true,
+  });
+
+  if (!temp) {
+    return (
+      <div className="flex justify-center items-center h-[70vh]">
+        <ClipLoader color="#EF3E37" size={75} aria-label="Loading..." />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-[100%] mb-5 md:h-screen flex justify-center relative">
+      <Gate />
+    </div>
+  );
+};
+
+export default ComingSoonPage;
