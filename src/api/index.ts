5be7e7d797a22ecae4e54e5c0558a57a95be5d2d
MohammadHH <mhmd98.home@gmail.com>
2024-01-18 12:28:57 +0200

    feat: integrate axios

---
+++ src/api/index.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { EmailVerificationBody, SignUpBody } from "./interfaces";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_END_POINT,
+});
+
+export async function signUp({ confirmPassword, ...restPayload }: SignUpBody) {
+  return axiosInstance.post("/auth/signup", restPayload);
+}
+
+export async function requestNewEmailVerification(
+  payload: EmailVerificationBody
+) {
+  return axiosInstance.post("/auth/request-new-verification-email", payload);
+}
+
+export { axiosInstance as axios };
---
+++ src/api/interfaces.ts
@@ -0,0 +1,16 @@
+export interface SignUpBody {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  gender: "MALE" | "FEMALE" | "OTHER";
+  dob: string;
+  country: string;
+  phoneNumber: string;
+  howDidYouHearAboutUs: string;
+  confirmPassword: string;
+}
+
+export interface EmailVerificationBody {
+  email: string;
+}
