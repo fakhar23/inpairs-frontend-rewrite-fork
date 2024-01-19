8773c85a856b67129e501e0b974df8962f66bcd3
MohammadHH <mhmd98.home@gmail.com>
2024-01-19 08:35:23 +0200

    x

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,13 +3,10 @@ import React from "react";
 import { useState } from "react";
 
 import { Poppins } from "next/font/google";
-import Image from "next/image";
 import Link from "next/link";
 
 import { useForm } from "react-hook-form";
 
-import HidePassword from "@/assets/hidePassword.svg";
-import ShowPassword from "@/assets/showPassword.svg";
 import { Input } from "@/components/Input";
 import { PublicNavbar } from "@/components/PublicNav";
 import FormsLayout from "@/layouts/FormsLayout";
@@ -21,54 +18,12 @@ const poppins = Poppins({
   weight: "400",
 });
 
-const LogIn = () => {
-  return (
-    <div className="relative flex flex-col justify-around">
-      <PublicNavbar />
-      <div className="flex justify-center items-center">
-        <FormsLayout>
-          <div
-            className={`md:flex md:flex-col md:relative md:justify-center md:items-center md:p-0 md:m-0 md:w-full ${poppins.className}`}
-          >
-            <LogInForm />
-            <div className="flex flex-col justify-start">
-              <p className="text-center text-xsmall">
-                Forgot your password?{" "}
-                <Link href="/reset-password">
-                  <span className=" text-red">Reset password</span>
-                </Link>
-              </p>
-              <p className="text-center text-xsmall">
-                Didn&apos;t receive verification email?{" "}
-                <Link href="/request-verification-email">
-                  <span className=" text-red">Request new email </span>
-                </Link>
-              </p>
-              <p className="text-center text-xsmall">
-                Don&apos;t have an account?{" "}
-                <Link href="/register">
-                  <span className=" text-red md:text-[12px]">
-                    Create an account
-                  </span>
-                </Link>
-              </p>
-            </div>
-          </div>
-        </FormsLayout>
-      </div>
-      <div />
-    </div>
-  );
-};
-
-export default LogIn;
-
 interface FormValues {
   email: string;
   password: string;
 }
 
-const LogInForm = () => {
+function LoginForm() {
   // TODO: Add state logic for showing this error on 401
   const [invalidCredentials, setInvalidCredentials] = useState(false);
   const [load, setLoad] = useState(false);
@@ -131,4 +86,44 @@ const LogInForm = () => {
       </div>
     </form>
   );
-};
+}
+
+export default function Login() {
+  return (
+    <div className="relative flex flex-col justify-around">
+      <PublicNavbar />
+      <div className="flex justify-center items-center">
+        <FormsLayout>
+          <div
+            className={`md:flex md:flex-col md:relative md:justify-center md:items-center md:p-0 md:m-0 md:w-full ${poppins.className}`}
+          >
+            <LoginForm />
+            <div className="flex flex-col justify-start">
+              <p className="text-center text-xsmall">
+                Forgot your password?{" "}
+                <Link href="/reset-password">
+                  <span className=" text-red">Reset password</span>
+                </Link>
+              </p>
+              <p className="text-center text-xsmall">
+                Didn&apos;t receive verification email?{" "}
+                <Link href="/request-verification-email">
+                  <span className=" text-red">Request new email </span>
+                </Link>
+              </p>
+              <p className="text-center text-xsmall">
+                Don&apos;t have an account?{" "}
+                <Link href="/register">
+                  <span className=" text-red md:text-[12px]">
+                    Create an account
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </div>
+        </FormsLayout>
+      </div>
+      <div />
+    </div>
+  );
+}
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEvent, HTMLInputTypeAttribute, useState } from "react";
+import { InputHTMLAttributes, useState } from "react";
 
 import Image from "next/image";
 
@@ -16,54 +16,41 @@ import hidePasswordIcon from "@/assets/hidePassword.svg";
 import passwordIcon from "@/assets/showPassword.svg";
 import { twMerge } from "tailwind-merge";
 
-interface FormHookConfig<T extends FieldValues> {
-  register: UseFormRegister<T>;
-  pattern?: RegisterOptions["pattern"];
-  validate?: RegisterOptions["validate"];
-}
-
-export interface InputFieldProps<T extends FieldValues> {
-  className?: string;
-  id: Path<T>;
-  type: HTMLInputTypeAttribute;
-  placeholder?: string;
-  errors?: FieldErrors<T>;
-  errorMessage?: string;
-  readOnly?: boolean;
-  defaultValue?: any;
-  formConfig?: FormHookConfig<T>;
-  max?: number;
-  min?: number;
-  name?: Path<T>;
-  label?: React.ReactNode;
-  value?: HTMLInputElement["value"];
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  variation?: "primary" | "secondary";
-}
+export type InputFieldProps<T extends FieldValues> =
+  InputHTMLAttributes<HTMLInputElement> & {
+    id: Path<T>;
+    errors?: FieldErrors<T>;
+    errorMessage?: string;
+    register?: UseFormRegister<T>;
+    pattern?: RegisterOptions["pattern"];
+    validate?: RegisterOptions["validate"];
+    name?: Path<T>;
+    label?: React.ReactNode;
+    variation?: "primary" | "secondary";
+  };
 
 export function Input<T extends FieldValues>({
   className,
-  id,
-  type,
-  placeholder,
   errors,
   errorMessage,
-  readOnly,
-  formConfig,
-  defaultValue,
-  max,
-  min,
-  name,
   label,
-  value,
   variation = "primary",
-  onChange,
+  register,
+  validate,
+  ...rest
 }: InputFieldProps<T>) {
+  const registeredProps = register
+    ? register(rest.id, {
+        required: errorMessage,
+        pattern: rest.pattern,
+        validate,
+      })
+    : {};
   const [showPassword, setShowPassword] = useState<boolean>(false);
   return (
     <div className={twMerge("w-full mb-2 relative", className)}>
       {label && (
-        <label htmlFor={id} className=" text-[#3D3C3C]">
+        <label htmlFor={rest.id} className=" text-[#3D3C3C]">
           {label}
         </label>
       )}
@@ -74,35 +61,20 @@ export function Input<T extends FieldValues>({
             "appearance-none border-b border-slate-400 text-gray-gunmetal leading-tight focus:outline-none h-[3rem] w-full focus:placeholder-transparent focus:border-red-500 bg-transparent md:h-[4rem] md:placeholder:text-[12px] md:text-[12px]",
           variation === "secondary" &&
             "bg-[#EFEFEF96] rounded-[10px] px-3 border-slate-400 mt-2 leading-tight h-[3rem] w-full md:h-[4rem] md:placeholder:text-[12px] md:text-[12px] text-[#5B5B5B] outline-none focus:outline-[#EF3E37]",
-          readOnly &&
+          rest.readOnly &&
             "bg-[#EFEFEF96] text-[#7b7b7b] cursor-not-allowed focus:outline-[#7b7b7b] focus:outline"
         )}
-        id={id}
-        type={showPassword ? "text" : type}
-        placeholder={placeholder}
-        readOnly={readOnly}
-        defaultValue={defaultValue}
-        max={max}
-        min={min}
-        value={value}
-        {...(formConfig
-          ? formConfig.register(id, {
-              required: errorMessage,
-              pattern: formConfig.pattern,
-              validate: formConfig.validate,
-            })
-          : {
-              name,
-              onChange,
-            })}
+        type={showPassword ? "text" : rest.type}
+        {...rest}
+        {...registeredProps}
       />
-      {errors && errors[id]?.message && (
+      {errors && errors[rest.id]?.message && (
         <p className="text-red text-[0.8rem]">
-          {JSON.stringify(errors[id]?.message)}
+          {JSON.stringify(errors[rest.id]?.message)}
         </p>
       )}
 
-      {type === "password" && (
+      {rest.type === "password" && (
         <div className="absolute right-2 top-[18px] bottom-2">
           <button
             className="text-sm"
