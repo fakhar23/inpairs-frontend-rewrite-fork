31b687b6050827d770975ffcb4f94b399fddb3a0
MohammadHH <mhmd98.home@gmail.com>
2024-01-27 00:23:55 +0200

    feat: add cloudinary image uploader

--- src/app/create/PersonalDetails.tsx
+++ src/app/create/PersonalDetails.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { PopupButton, Widget } from "@typeform/embed-react";
-import { CldUploadWidget } from "next-cloudinary";
 
 import about from "@/assets/About Yourself.svg";
 import demographics from "@/assets/Demographics.svg";
 import personal from "@/assets/Personal Test.svg";
-import { Card } from "@/components";
+import { Card, Modal } from "@/components";
 import { useAuthContext } from "@/hooks/useAuthContext";
+import ImagesUploader from "./ImagesUploader";
+import { useState } from "react";
 
 const PersonalDetails = () => {
+  const [isImageModalOpened, setIsImageModalOpened] = useState<boolean>(false);
   const user = useAuthContext();
 
   const {
@@ -21,6 +23,8 @@ const PersonalDetails = () => {
     firstName,
     lastName,
     dateOfBirth,
+    completedFirstForm,
+    completedSecondForm,
   } = user.data || {};
   const handleSubmit = () => {
     // TODO: Backend
@@ -29,6 +33,13 @@ const PersonalDetails = () => {
   // preset
   return (
     <div className="relative">
+      <Modal
+        isOpen={isImageModalOpened}
+        onClose={() => setIsImageModalOpened(false)}
+      >
+        <ImagesUploader onClose={() => setIsImageModalOpened(false)} />
+      </Modal>
+
       <div className="my-0 mx-auto flex flex-col gap-[5rem] items-center md:flex-col-reverse">
         <section className="flex gap-4 justify-center w-[100%] md:flex-col md:items-center md:justify-center">
           <PopupButton
@@ -47,53 +58,53 @@ const PersonalDetails = () => {
             }}
             disabled={true}
           >
-            <Card title="Demographics" icon={demographics} />
+            <Card
+              title="Demographics"
+              icon={demographics}
+              loading={user.isLoading}
+              disabled={completedFirstForm || user.isLoading}
+              isCompleted={completedFirstForm}
+            />
           </PopupButton>
 
           <PopupButton
-            id={process.env.NEXT_PUBLIC_FORM_TWO_ID}
+            id={process.env.NEXT_PUBLIC_FORM_TWO_ID as string}
             size={90}
             className="flex justify-center w-[100%]"
             onSubmit={handleSubmit}
-            // hidden={{ user_id: user?.sharableId }}
+            hidden={{
+              user_id: uid,
+              email,
+              gender,
+              phone_number: phone,
+              first_name: firstName,
+              last_name: lastName,
+              date_of_birth: dateOfBirth,
+            }}
           >
-            <Card title="Personal Test" icon={personal} />
+            <Card
+              title="Personal Test"
+              icon={personal}
+              loading={user.isLoading}
+              disabled={completedSecondForm || user.isLoading}
+              isCompleted={completedSecondForm}
+            />
           </PopupButton>
 
           <Card
             title={
               <>
+                <div>Upload Pictures</div>
                 <span className="text-rose-500 text-regular ml-2">
                   {numberOfUploadedImages}/3 uploaded
                 </span>
-                <CldUploadWidget
-                  uploadPreset={
-                    process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME as string
-                  }
-                  options={{
-                    clientAllowedFormats: [
-                      "jpg",
-                      "jpeg",
-                      "png",
-                      "heif",
-                      "heic",
-                    ],
-                    multiple: true,
-                    maxFiles: 3,
-                    sources: ["local", "camera"],
-                    showPoweredBy: false,
-                  }}
-                >
-                  {({ open, results }) => {
-                    results?.info;
-                    return (
-                      <button onClick={() => open()}>Upload Pictures</button>
-                    );
-                  }}
-                </CldUploadWidget>
               </>
             }
+            loading={user.isLoading}
+            disabled={numberOfUploadedImages === 3 || user.isLoading}
+            isCompleted={numberOfUploadedImages === 3}
             icon={about}
+            onClick={() => setIsImageModalOpened(true)}
           />
         </section>
       </div>
