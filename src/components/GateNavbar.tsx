62d61a6d7ce03db71d5803b18df10792c20da349
MohammadHH <mhmd98.home@gmail.com>
2024-02-06 02:09:30 +0200

    fix: fix gate nav mobile menu

--- src/components/GateNavbar.tsx
+++ src/components/GateNavbar.tsx
@@ -8,10 +8,18 @@ import { usePathname } from "next/navigation";
 import { LinkButton } from "@/components";
 
 import logo from "@/assets/pears-inline.png";
-import { MenuIcon } from "../Icons";
+import { CloseIcon, MenuIcon } from "../Icons";
 import { useQuery } from "@tanstack/react-query";
 import { useAuthContext } from "@/hooks/useAuthContext";
 import { AuthContextResponse } from "@/api/types";
+import { useClickOutside } from "@mantine/hooks";
+import { useSpring, animated } from "react-spring";
+
+interface IMenuProps {
+  isOpen: boolean;
+  onClose: () => void;
+  canViewTheirProfile?: boolean;
+}
 
 function getLink(userContext: AuthContextResponse | undefined) {
   const links = {
@@ -77,6 +85,78 @@ function getLink(userContext: AuthContextResponse | undefined) {
   else throw new Error("Unsupported state");
 }
 
+const Menu = ({ isOpen, onClose, canViewTheirProfile = false }: IMenuProps) => {
+  const props = useSpring({
+    to: {
+      opacity: isOpen ? 1 : 0,
+      transform: `translateX(${isOpen ? 0 : 100}%)`,
+    },
+    from: { opacity: 0, transform: "translateX(100%)" },
+    config: { duration: 200, clamp: true },
+  });
+
+  const closeOnOutsideClickRef = useClickOutside(onClose);
+
+  return (
+    <animated.div
+      style={props}
+      className={`fixed z-10 shadow-md bg-white top-0 right-0 p-[1.5rem] w-[50%] h-[100vh] sm:w-[70%]
+          space-y-10 text-right`}
+      ref={closeOnOutsideClickRef}
+    >
+      <button
+        className="cursor-pointer px-12"
+        title="closing button"
+        onClick={onClose}
+      >
+        <CloseIcon size={"2.5em"} />
+      </button>
+
+      <ul className="text-[1.5rem] text-left []">
+        <Link href="/">
+          <li className="hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+            <span onClick={onClose}>Home</span>
+          </li>
+        </Link>
+
+        <Link href="how">
+          <li className="hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+            <span onClick={onClose}>How does it work?</span>
+          </li>
+        </Link>
+
+        <Link href="about">
+          <li className="hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+            <span onClick={onClose}>About us</span>
+          </li>
+        </Link>
+
+        <Link href="contact">
+          <li className="hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+            <span onClick={onClose}>Contact us</span>
+          </li>
+        </Link>
+
+        <Link href="FAQ">
+          <li className="hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+            <span onClick={onClose}>FAQ</span>
+          </li>
+        </Link>
+
+        <li className=" ml-auto md:ml-0 md:border-none hover:bg-[#ef3e37] py-4 my-1 px-4 hover:bg-opacity-90 transition-all duration-300 rounded-md">
+          {canViewTheirProfile ? (
+            <Link href={`/profile/me`}>
+              <span onClick={onClose}> Profile </span>
+            </Link>
+          ) : (
+            <Link href="/login"> Sign in </Link>
+          )}
+        </li>
+      </ul>
+    </animated.div>
+  );
+};
+
 export function GateNavbar() {
   const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
 
@@ -103,17 +183,6 @@ export function GateNavbar() {
           priority={true}
         />
       </Link>
-
-      <button
-        type="button"
-        title="menu"
-        className="items-center p-2 text-neutral-500 rounded-lg hidden md:inline-flex hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:ring-gray-600"
-        onClick={() => {
-          setIsMenuOpened(!isMenuOpened);
-        }}
-      >
-        <MenuIcon />
-      </button>
 
       <ul
         className={`font-bryantProMedium  flex justify-between items-center  gap-[3rem] [&>*]:cursor-pointer [&>*]:text-[1.2rem] text-gray-charcoal list-none md:hidden`}
@@ -146,6 +215,23 @@ export function GateNavbar() {
       >
         {getLink(user.data)}
       </Skeleton>
+
+      <button
+        type="button"
+        title="menu"
+        className="items-center p-2 text-neutral-500 rounded-lg hidden md:inline-flex hover:bg-neutral-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:ring-gray-600"
+        onClick={() => {
+          setIsMenuOpened(!isMenuOpened);
+        }}
+      >
+        <MenuIcon />
+      </button>
+
+      <Menu
+        isOpen={isMenuOpened}
+        onClose={() => setIsMenuOpened(false)}
+        canViewTheirProfile={user.data?.canViewTheirProfile}
+      />
     </div>
   );
 }
