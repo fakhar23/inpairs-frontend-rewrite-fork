94f00a85eafe76f955867dad2850fad7f12dea99
mahdiAbu3li <mahdiimad64@gmail.com>
2024-02-08 17:02:52 +0300

    fix: settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -161,27 +161,33 @@ const ProfileSettings = () => {
     <UserProfileLayout>
       <SettingsLayout>
         {/* <EditProfile answers={answersObject} />; */}
-        <Toast
-          type="warn"
-          message={
-            <p className="text-2xl">
-              This page is still under construction. If you’d like to modify any
-              of your profile settings, send an email to
-              <a
-                href="mailto:zachariah@inpairs.io"
-                rel="noreferrer"
-                target="_blank"
-                className="underline text-blue-500  inline-flex items-end gap-1"
-              >
-                <span className="ml-1">
-                  <MdEmail />
-                </span>
-                zachariah@inpairs.io
-              </a>{" "}
-              !
-            </p>
-          }
-        />
+        <section className="w-[100%] p-10 py-[3rem] my-0 mx-auto md:w-[100%]">
+          <h2 className="text-[2.5rem] text-title font-bold mb-[2rem]">
+            Profile Settings
+          </h2>
+
+          <Toast
+            type="warn"
+            message={
+              <p className="text-2xl">
+                This page is still under construction. If you’d like to modify
+                any of your profile settings, send an email to
+                <a
+                  href="mailto:zachariah@inpairs.io"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="underline text-blue-500  inline-flex items-end gap-1"
+                >
+                  <span className="ml-1">
+                    <MdEmail />
+                  </span>
+                  zachariah@inpairs.io
+                </a>{" "}
+                !
+              </p>
+            }
+          />
+        </section>
       </SettingsLayout>
     </UserProfileLayout>
   );
