948e13cb0f75b213fea2a49074c131265980ff2e
fakhar23 <muhammadfakhar111@gmail.com>
2024-02-15 20:49:46 +0500

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 import footerBG from "@/assets/footerBG.svg";
+import Link from "next/link";
+import logo from "@/assets/pears-inline.png";
 
 interface IFooter {
   className?: string;
@@ -12,15 +14,55 @@ export function Footer({ className }: IFooter) {
   return (
     <footer
       className={twMerge(
-        "px-[5rem] py-[2rem] h-[15rem] relative bg-gradient-to-r from-primary/90 to-secondary/90 flex gap-[2rem] justify-between items-center text-white md:flex-col md:m-0 ",
+        "px-[5rem] md:p-[2rem] py-[2rem] relative text-gray-400 md:m-0 bg-gray-800 ",
         className
       )}
     >
-      <Image
-        src={footerBG}
-        alt="art"
-        className="absolute top-0 left-0 w-[100%] h-[100%] object-cover object-top"
-      />
+      <div className="grid grid-cols-2 md:grid-cols-1 gap-[2rem] w-full border-b-2 border-gray pb-8">
+        <div className="">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="pairs logo"
+              className="w-[7rem] mb-4"
+              priority={true}
+            />
+          </Link>
+          <p>
+            Inpairs is a service that brings the 21st <br />
+            century to Muslim matchmaking.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-1 gap-[2rem] w-fit">
+          <div>
+            <p className="text-white mb-4 text-lg">Social Media</p>
+            <Link
+              className="block mb-2"
+              href={"https://www.instagram.com/inpairs.io/"}
+            >
+              Instagram
+            </Link>
+            <Link
+              className="block mb-2"
+              href={"https://www.tiktok.com/@zachelkordy"}
+            >
+              Tiktok
+            </Link>
+          </div>
+          <div>
+            <p className="text-white mb-4 text-lg">Contact Us</p>
+            <Link className="block mb-2" href={"mailto:zachariah@inpairs.io"}>
+              zachariah@inpairs.io
+            </Link>
+            <Link className="block mb-2" href={"tel:+12345678890"}>
+              +1234 5678 890
+            </Link>
+          </div>
+        </div>
+      </div>
+      <p className="py-8">
+        Copyright © 2024 Inpairs Team. All Rights Reserved.
+      </p>
     </footer>
   );
 }
